51a2b6b649d1eb39c885d8b6e09ef095dc41dd5b
Haru <e1232580@gmail.com>
2025-02-21 23:05:51 +0800

    新增navlink

--- src/layouts/FrontLayout.jsx
+++ src/layouts/FrontLayout.jsx
@@ -1,16 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
+
+const routes = [
+    { path: "/", name: "首頁" },
+    { path: "/products", name: "產品列表" },
+    { path: "/cart", name: "購物車" },
+  ];
+
 
 export default function FrontLayout () {
+
+    const navLinK = routes.map((route) => (
+        <li key={route.path} className="nav-item">
+            <NavLink className="nav-link" aria-current="page" to={route.path}>{route.name}</NavLink>
+        </li>
+    ));
+
     return (
     <>
         <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
             <div className="container">
                 <ul className="navbar-nav flex-row gap-5 fs-5">
-                <li className="nav-item">
-                    <a className="nav-link" aria-current="page" href="#">首頁</a>
-                </li>
-                <li className="nav-item"><a className="nav-link" href="#">產品頁</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">購物車</a></li>
+                    {navLinK}
                 </ul>
             </div>
         </nav>
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,5 @@
+export default function ProductsPage (){
+    return (
+        <h1>這是產品頁面</h1>
+    )
+} 
