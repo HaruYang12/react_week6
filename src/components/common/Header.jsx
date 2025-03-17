17591a1c2bdba50288d2dc1234146fbc0af1db4a
Haru <e1232580@gmail.com>
2025-03-17 11:00:40 +0800

    修改版型

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,3 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+const routes = [
+  { path: "/", name: "首頁" },
+  { path: "/products", name: "產品列表" },
+  { path: "/products/:id", name: "產品頁面" },
+  { path: "/cart", name: "購物車" },
+  { path: "/login", name: "登入" }
+];
+
 export default function Header() {
     return (
         <div
@@ -26,12 +36,12 @@ export default function Header() {
                 <a className="nav-item nav-link me-4 active" href="./index.html">
                   Home <span className="sr-only">(current)</span>
                 </a>
-                <a className="nav-item nav-link me-4" href="./product.html">
+                <NavLink className="nav-item nav-link me-4" to="/products">
                   Product
-                </a>
-                <a className="nav-item nav-link me-4" href="./detail.html">
+                </NavLink>
+                <NavLink className="nav-item nav-link me-4" to="/detail">
                   Detail
-                </a>
+                </NavLink>
                 <a className="nav-item nav-link" href="./cart.html">
                   <i className="fas fa-shopping-cart"></i>
                 </a>
--- src/layouts/FrontLayout.jsx
+++ src/layouts/FrontLayout.jsx
@@ -1,31 +1,19 @@
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet} from "react-router-dom";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
 
-const routes = [
-    { path: "/", name: "首頁" },
-    { path: "/products", name: "產品列表" },
-    { path: "/cart", name: "購物車" },
-    { path: "/login", name: "登入" }
-  ];
+
 
 
 export default function FrontLayout () {
 
-    const navLinK = routes.map((route) => (
-        <li key={route.path} className="nav-item">
-            <NavLink className="nav-link" aria-current="page" to={route.path}>{route.name}</NavLink>
-        </li>
-    ));
+
 
     return (
     <>
-        <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-            <div className="container">
-                <ul className="navbar-nav flex-row gap-5 fs-5">
-                    {navLinK}
-                </ul>
-            </div>
-        </nav>
+        <Header />
         <Outlet />
+        <Footer />
     </>
     )
 }
